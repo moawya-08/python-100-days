e54496e5c8d480a3c22661969c841a4a1b2dd0d3
moawya-08 <moawyamahmoud@gmail.com>
2026-08-13 00:25:19 +0530

    Day 8: Caeser Cipher

--- Day08/Caeser_Cipher.py
+++ Day08/Caeser_Cipher.py
@@ -1,27 +1,29 @@
 from art import logo
 alphabet =[ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+print(logo)
 
 def caesar(direction,text,shift):
+    output_text = ""
     if direction == "decode":
-        shift *= -1
-    print(f"Your {direction}d text is: ")
-    
+            shift *= -1
     for letter in text:
-        shifted_position = alphabet.index(letter)+shift
-        output_text= (alphabet[shifted_position])
-        print(output_text,end="")
         if letter not in alphabet:
             output_text += letter
-    print("\n")
-        
-print(logo)
+        else:
+            shifted_position = alphabet.index(letter)+shift
+            output_text= (alphabet[shifted_position])
+            print(output_text,end="")
+    print("\n")     
+    print(f"Your {direction}d text is: ")
+
 should_continue = True
 while should_continue:
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: \n").lower()
     text = input("Type your message: ")
     shift = int(input("Type the shift number: "))
-    should_continue = input("Type 'yes' if you want to go again. Otherwie type 'no'.")
     caesar(direction,text,shift)
+    should_continue = input("Type 'yes' if you want to go again. Otherwie type 'no'.")
+
     if should_continue == 'no':
         should_continue = False
         print("Goodbye!")
